164a7fa7fb38a79cbf0723521c9c22d0935e7261
contrats-cee <contact@lecalumetdelapaie.com>
2025-11-17 03:19:01 +0100

    Update pdf.service.ts

--- backend/src/modules/pdf/pdf.service.ts
+++ backend/src/modules/pdf/pdf.service.ts
@@ -7,7 +7,10 @@ export class PdfService{
   async htmlToPdf(html: string): Promise<Buffer>{
     let browser
     try{
-      browser = await puppeteer.launch({ args:["--no-sandbox","--disable-setuid-sandbox"] })
+      browser = await puppeteer.launch({
+  headless: 'new', // force le mode headless moderne
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
       const page = await browser.newPage()
       await page.setContent(html, { waitUntil: 'networkidle0' })
       const pdf = await page.pdf({ format:'A4', printBackground:true, margin:{ top:'20mm', right:'15mm', bottom:'20mm', left:'15mm' } })
